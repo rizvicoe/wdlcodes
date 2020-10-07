ebebbe515932be3d116671e7c06170e002a61321
Shiburaj Pappu <powerupneo@gmail.com>
2020-10-07 20:57:35 +0530

    Javascript Week 10 part 1 code updated

---
+++ js1/main.js
@@ -0,0 +1,50 @@
+
+//alert("Hi");
+//document.write("<h1>Hello</h1>");
+//console.log('World');
+
+var x = 'Shibu';
+var y = 'Raj';
+var z = x+y;
+//console.log(z);
+
+/*if(x==5){
+    console.log('Hello');
+}else if(x==6){
+    console.log('World');
+}else if(x==7){
+    console.log('Severn');
+}else{
+    console.log('Else');
+}*/
+
+//x++;
+
+/*switch(x){
+    case 5:
+        console.log('Hello');
+        break;
+    case 6:
+        console.log('World');
+        break;
+    case 7:
+        console.log('Severn');
+        break;
+    default:
+        console.log('Else');
+}*/
+
+let i=1;
+for(; i<=10 ; i++){
+    console.log("i = " + i);
+}
+let i=4;
+console.log(i);
+
+/*var i=1;
+do{
+
+    console.log("i = " + i);
+    i++;
+}while(i<=10);*/
+    
